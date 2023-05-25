930c7b373c6a319ee17ea6e36c202e7539e1bef5
Wijkarn <mathias.wijk@hotmail.com>
2023-05-25 10:49:23 +0200

    added all files

--- src/main/java/org/example/CreateTables.java
+++ src/main/java/org/example/CreateTables.java
@@ -1,2 +1,57 @@
-package org.example;public class CreateTables {
+package org.example;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreateTables extends Model {
+
+    public static void createTest() {
+        try {
+            Connection connection = Model.getConnection();
+
+            String accounts = """
+                    CREATE TABLE IF NOT EXISTS `accounts` (
+                      `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+                      `owner_id` int NOT NULL,
+                      `created` datetime DEFAULT (now()),
+                      `balance` int NOT NULL,
+                      `account_id` int NOT NULL,
+                      UNIQUE KEY `id` (`id`)
+                    )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;""";
+            Statement account = connection.createStatement();
+            account.executeUpdate(accounts);
+
+            String trans = """
+                    CREATE TABLE IF NOT EXISTS `transactions` (
+                      `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+                      `from` int NOT NULL,
+                      `to` int NOT NULL,
+                      `amount` int NOT NULL,
+                      `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                      UNIQUE KEY `id` (`id`)
+                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;""";
+            Statement transac = connection.createStatement();
+            transac.executeUpdate(trans);
+
+            String users = """
+                    CREATE TABLE `users` (
+                      `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+                      `personnummer` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+                      `email` varchar(50) NOT NULL,
+                      `phone` varchar(20) NOT NULL,
+                      `password` varchar(100) NOT NULL,
+                      `address` varchar(100) NOT NULL,
+                      `name` varchar(50) NOT NULL,
+                      `created` datetime DEFAULT (now()),
+                      UNIQUE KEY `id` (`id`)
+                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;""";
+            Statement user = connection.createStatement();
+            user.executeUpdate(users);
+
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/org/example/Model.java
+++ src/main/java/org/example/Model.java
@@ -1,2 +1,47 @@
-package org.example;public class Model {
+package org.example;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import java.sql.*;
+
+public abstract class Model {
+    private static MysqlDataSource dataSource;
+
+    static String url = "localhost";
+    static int port = 3306;
+    static String database = "SlutProjekt";
+    static String username = "root";
+    static String password = "Omegalul69";
+
+    protected Model() {
+
+    }
+
+    private static void initializeDataSource() {
+        try {
+            dataSource = new MysqlDataSource();
+            dataSource.setUser(username);
+            dataSource.setPassword(password);
+            dataSource.setUrl("jdbc:mysql://" + url + ":" + port + "/" + database + "?serverTimezone=UTC");
+            dataSource.setUseSSL(false);
+        } catch (SQLException e) {
+            System.out.println("Initialize Failed");
+        }
+    }
+
+    //Skapar en tillfällig koppling till databasen
+    protected static Connection getConnection() {
+        try {
+            initializeDataSource();
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            System.out.println("failed!");
+            System.exit(0);
+            return null;
+        }
+    }
+
+    protected MysqlDataSource getDataSource() {
+        return dataSource;
+    }
 }
