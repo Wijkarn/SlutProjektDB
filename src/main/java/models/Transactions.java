61422106df1dbc001fb4bed89860eb4b4900117e
Wijkarn <mathias.wijk@hotmail.com>
2023-05-29 17:04:38 +0200

    Added transactions class

---
+++ src/main/java/models/Transactions.java
@@ -0,0 +1,54 @@
+package models;
+
+import java.sql.Timestamp;
+
+public class Transactions {
+    private int transactionId;
+    private int receiverId;
+    private int senderId;
+    private int amount;
+    private Timestamp date;
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public int getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(int receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public int getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(int senderId) {
+        this.senderId = senderId;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    public Transaction(){
+        
+    }
+}
